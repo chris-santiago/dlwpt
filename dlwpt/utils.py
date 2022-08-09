0b801a4225cff48368766c635e2c1525f7e4048e
chris-santiago <cjsantiago@gatech.edu>
2022-08-08 23:13:08 -0400

    add lang dataset

--- dlwpt/utils.py
+++ dlwpt/utils.py
@@ -1,3 +1,10 @@
+import io
+import string
+import unicodedata
+import zipfile
+import re
+
+import requests
 import torch
 from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
@@ -16,3 +23,42 @@ def get_mnist_datasets():
     train = MNIST(DATA_DIR, train=True, download=True, transform=transforms.ToTensor())
     test = MNIST(DATA_DIR, train=False, download=True, transform=transforms.ToTensor())
     return train, test
+
+
+def get_alphabet():
+    letters = string.ascii_letters + ".,;'"
+    alphabet = dict(enumerate(letters))
+    return letters, alphabet
+
+
+def get_language_data(verbose=False):
+    url = 'https://download.pytorch.org/tutorial/data.zip'
+    resp = requests.get(url)
+    contents = zipfile.ZipFile(io.BytesIO(resp.content))
+    contents.extractall()
+
+    data = {}
+    letters = string.ascii_letters + ".,;'"
+    alphabet = dict(enumerate(letters))
+
+    def to_ascii(s):
+        return ''.join(
+            c for c in unicodedata.normalize('NFD', s)
+            if unicodedata.category(c) != 'Mn'
+            and c in letters
+        )
+
+    for fp in contents.namelist():
+        if 'data/names/' in fp and fp.endswith('.txt'):
+            lang = re.search(r'([A-Z]+)\w+', fp)[0]
+            with contents.open(fp) as file:
+                lang_names = [
+                    to_ascii(line).lower()
+                    for line in str(file.read(), encoding='utf-8').strip().split('\n')
+                ]
+                data[lang] = lang_names
+            if verbose:
+                print(f'{lang}: {len(lang_names)}')
+
+    return data
+
